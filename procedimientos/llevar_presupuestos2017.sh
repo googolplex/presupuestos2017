879b1852128923648eddb5694a0208bcf5fb8e53
googol plex <googolplex@lcompras.biz>
2017-10-28 11:35:38 -0300

    presupuestos version 9 - agrego el informe

--- procedimientos/llevar_presupuestos2017.sh
+++ procedimientos/llevar_presupuestos2017.sh
@@ -2,6 +2,9 @@
 cp *.sh presupuestos2017/procedimientos
 rm -R presupuestos2017/*.*~
 cp -R /home/anaramos/openxava-5.4.1/workspace/Presupuestos/* presupuestos2017/presupuestos_version_anaramos2016
+cp -R /home/anaramos/openxava-5.4.1/tomcat/conf/context.xml presupuestos2017/tomcatfeliz/
+cp -R /home/anaramos/openxava-5.4.1/tomcat/conf/server.xml presupuestos2017/tomcatfeliz/
+cp -R /home/anaramos/openxava-5.4.1/tomcat/conf/web.xml presupuestos2017/tomcatfeliz/
 cd presupuestos2017
 git config --global user.email "googolplex@lcompras.biz"
 git config --global user.name "googol plex"
